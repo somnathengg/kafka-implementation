f1d0ae639adb085914ef9fccf6d89750ce3ddf9e
somnathengg <somnath8383@gmail.com>
2025-11-10 22:54:14 +0530

    changes for seperate consumer

--- kafka-producer/src/main/java/com/kafka/producer/config/KafkaProducerConfig.java
+++ kafka-producer/src/main/java/com/kafka/producer/config/KafkaProducerConfig.java
@@ -1,12 +1,14 @@
-package com.kafka.config;
+package com.kafka.producer.config;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -15,17 +17,26 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @Configuration
 public class KafkaProducerConfig {
 
-	@Bean
-	public ProducerFactory<String, String> producerFactory() {
-		Map<String, Object> configProps = new HashMap<>();
-		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-		return new DefaultKafkaProducerFactory<>(configProps);
-	}
+//	@Bean
+//	public ProducerFactory<String, String> producerFactory() {
+//		Map<String, Object> configProps = new HashMap<>();
+//		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+//		return new DefaultKafkaProducerFactory<>(configProps);
+//	}
 
+//	@Bean
+//	public KafkaTemplate<String, String> kafkaTemplate() {
+//		return new KafkaTemplate<>(producerFactory());
+//	}
+	
+	
 	@Bean
-	public KafkaTemplate<String, String> kafkaTemplate() {
-		return new KafkaTemplate<>(producerFactory());
+	public NewTopic createTopic() {
+	    return TopicBuilder.name("latest-topic")
+	            .partitions(3)
+	            .replicas(1)
+	            .build();
 	}
 }
